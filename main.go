2ab8134790a5d202492add2135f7729cf26e1ebc
Virus.V <virusv@live.com>
2018-05-22 20:20:20 +0800

    修改使用方法，不可以启用Linux内核自带的IP转发

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ var (
 	flagInterface    = flag.String("interface", "wlp5s0", `本机网络接口`)
 	flagTarget       = flag.String("target", "", `目标主机。 "1.2.3.4" 或者 "1.2.3.0/24" 或者 "1.2.3-7.4-12" 或者 "1.2.3.*" 或者前面几种的集合 "1.2.3.4, 1.2.3.0/24, ..."`)
 	flagWaitInterval = flag.Float64("wait", 2.0, `每次广播之间等待时间，单位是秒 必须大于 0.1`)
-	flagDNSConfig    = flag.String("dnscfg", "", "DNS欺骗配置文件，配置要对哪些域名欺骗")
-	flagGateWay      = flag.String("gateway", "192.168.1.1", "网关的IP地址")
+	flagDNSConfig    = flag.String("dnscfg", "", `DNS欺骗配置文件，配置要对哪些域名欺骗`)
+	flagGateWay      = flag.String("gateway", "192.168.1.1", `网关的IP地址`)
 	flagHelp         = flag.Bool("help", false, `打印本使用方法`)
 )
 
@@ -35,7 +35,8 @@ func main() {
 		fmt.Println("该代码欺骗局域网中指定的目标")
 		fmt.Println("利用ARP协议，将特制的ARP包发送到指定目标主机，篡改目标的缓存表，把目标之间所有流量都转到我电脑上")
 		fmt.Println("")
-		fmt.Println("在此之前Linux要启用IP转发（搞过软路由的应该秒懂），可以很大程度减小被发现的几率：sysctl -w net.ipv4.ip_forward=1")
+		// fmt.Println("在此之前Linux要启用IP转发（搞过软路由的应该秒懂），可以很大程度减小被发现的几率：sysctl -w net.ipv4.ip_forward=1")
+		fmt.Println("切记不要打开Linux内核自带的IP转发功能。执行“sysctl -w net.ipv4.ip_forward=0”关闭该功能")
 		fmt.Println("")
 		fmt.Println("Usage: ")
 		fmt.Println(os.Args[0], "[-interface <interface>] -target <target host>")
@@ -73,15 +74,6 @@ func main() {
 			log.Fatalf("解析DNS欺骗配置文件出错：%s\n", err)
 		}
 	}
-
-	// for _, v := range Zebra.Targets {
-	// 	fmt.Println(v.Address)
-	// 	for _, s := range v.Matches {
-	// 		fmt.Println(s)
-	// 	}
-	// }
-
-	// return
 
 	// 解析目标ip列表
 	addrRange, err := iprange.ParseList(*flagTarget)
@@ -152,9 +144,7 @@ func doARPLookup(ip net.IP) (net.HardwareAddr, error) {
 	lineMatch := regexp.MustCompile(`\?\s+\(([0-9\.]+)\)\s+at\s+([0-9a-f:]+).+on\s+([^\s]+)`)
 	// 匹配ARP缓存条目
 	matches := lineMatch.FindAllStringSubmatch(string(out), 1)
-	//matches[0][0] = IP地址
-	//matches[0][1] = MAC地址
-	//matches[0][2] = 网卡名
+
 	if len(matches) > 0 && len(matches[0]) > 3 {
 		//ipAddr := net.ParseIP(matches[0][1])
 		fmt.Println(matches[0][2])
@@ -164,11 +154,6 @@ func doARPLookup(ip net.IP) (net.HardwareAddr, error) {
 		if err != nil {
 			return nil, fmt.Errorf("解析MAC失败：%v", err)
 		}
-
-		//iface, err := net.InterfaceByName(matches[0][3])
-		//if err != nil {
-		//	return nil, fmt.Errorf("无法找到接口: %v", err)
-		//}
 
 		return macAddr, nil
 	}
